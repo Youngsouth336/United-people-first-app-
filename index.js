d9dd70a80a08ae8c687618fbe867f444e9d2d48f
Youngsouth336 <michaelpattersonbook@gmail.com>
2025-07-07 12:58:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// index.js
-console.log('Hello from United People First App');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from United People First App!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
