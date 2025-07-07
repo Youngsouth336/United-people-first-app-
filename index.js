e3b91553096656212b51c5bcc3afa37039acba73
Youngsouth336 <michaelpattersonbook@gmail.com>
2025-07-07 13:25:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(__dirname));
-
-// Serve index.html when user accesses "/"
+// Serve index.html from the root
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
